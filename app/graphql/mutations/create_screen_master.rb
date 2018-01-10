7f60f41b3a6c74ccb82dd1cd251c1c456bbbf3da
arps1989dev <arpita.sojitra@gmail.com>
2018-01-10 14:45:27 +0530

    id fetch nil

--- app/graphql/mutations/create_screen_master.rb
+++ app/graphql/mutations/create_screen_master.rb
@@ -4,12 +4,11 @@ class Mutations::CreateScreenMaster < GraphQL::Function
   type Types::ScreenMasterType
   def call(obj, args, ctx)
     binding.pry
-    model = ScreenMaster.new(args[:screen_master].to_h)
-    if (ctx[:current_ability].can?(:create, model))
+    if (ctx[:current_ability].can?(:create, obj))
       persist_model = ScreenMaster.create args[:screen_master].to_h
       persist_model.roles << ctx[:current_resource_owner].roles.first
     else
-      model
+    model = ScreenMaster.new(args[:screen_master].to_h)
     end
   end
 
